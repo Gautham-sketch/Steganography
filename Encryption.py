d59a341f69d654c537b344c3714d868fe211f309
Gautham-sketch <82305202+Gautham-sketch@users.noreply.github.com>
2023-04-18 19:07:19 +0530

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -1,15 +1,14 @@
-
 import wave
 
 song = wave.open("song.wav", mode='rb')
 frame_bytes = bytearray(list(song.readframes(song.getnframes())))
 string =input ("Enter your Message ")
 
-#string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
-#bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in string])))
-#for i, bit in enumerate(bits):
-    #frame_bytes[i] = (frame_bytes[i] & 254) | bit
-#frame_modified = bytes(frame_bytes)
+string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
+bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in string])))
+for i, bit in enumerate(bits):
+    frame_bytes[i] = (frame_bytes[i] & 254) | bit
+frame_modified = bytes(frame_bytes)
 
 #string = string + int((len(frame_bytes)-(len(string)*8*8))/8) *'#'
 #bits = list(map(int, ''.join([bin(ord(i)).lstrip('0b').rjust(8,'0') for i in string])))
